99f40e92e111d8f4e1c2b26c5b6c3c57f4eb7d9c
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-04-30 11:29:58 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -49,14 +49,14 @@ resource "aws_security_group" "fromWebServer" {
 } 
 output "ami-ouput" {
     description = "My ami output"
-    value = data.myWebServer.id
+    value = aws_instance.myWebServer.id
 }
 
 output "ami-ouput-creation-date" {
     description = "My ami output creation date"
-    value = data.aws_ami.myWebServer.creation_date
+    value = aws_instance.myWebServer.creation_date
 }
 output "instance_public_ip" {
   description = "Public IP address of the EC2 instance"
-  value       = data.aws_ami.myWebServer.app_server.public_ip
+  value       = aws_instance.myWebServer.public_ip
 }
