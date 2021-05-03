2ef76543e3b6bfe7b27b9c2a34d2356311b37fb6
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-03 17:31:50 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,7 +3,22 @@
 #     value = aws_instance.myWebServer.id
 # }
 
-# output "instance_public_ip" {
-#   description = "Public IP address of the EC2 instance"
-#   value       = aws_instance.myWebServer.public_ip
-# }
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = data.latest_ubuntu.public_ip[0]
+}
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = data.latest_ubuntu.public_ip[1]
+}
+output "web_loadbalancer_url" {
+  value = aws_elb.web.dns_name
+}
+
+output "latest_ubuntu_ami_id" {
+  value = data.aws_ami.latest_ubuntu.id
+}
+
+output "latest_ubuntu_ami_name" {
+  value = data.aws_ami.latest_ubuntu.name
+}
