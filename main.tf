c3a875c3c2d49185b282024be1e2e04acd7a87e2
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-04 15:28:45 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -164,21 +164,22 @@ resource "aws_autoscaling_group" "web" {
 
 resource "aws_elb" "web" {
   name               = "WebServer-HA-ELB"
+  load_balancer_type = "application"
   availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
   security_groups    = [aws_security_group.webSG.id]
-  listener {
-    lb_port           = 80
-    lb_protocol       = "http"
-    instance_port     = 80
-    instance_protocol = "http"
-  }
-  health_check {
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-    timeout             = 3
-    target              = "HTTP:80/"
-    interval            = 10
-  }
+#   listener {
+#     lb_port           = 80
+#     lb_protocol       = "http"
+#     instance_port     = 80
+#     instance_protocol = "http"
+#   }
+#   health_check {
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#     timeout             = 3
+#     target              = "HTTP:80/"
+#     interval            = 10
+#   }
   tags = {
     Name = "WebServer-Highly-Available-ELB"
   }
