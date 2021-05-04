19b0a6ac507798fa5c4b2f7b31db1415557dde74
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-04 12:20:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -41,33 +41,108 @@ resource "aws_security_group" "webSG" {
   }
 }
 
+resource "aws_launch_template" "web" {
+  name = "web"
 
-resource "aws_launch_configuration" "web" {
-  //  name            = "WebServer-Highly-Available-LC"
-  name_prefix     = "WebServer-Highly-Available-LC-"
-  image_id        = data.aws_ami.latest_ubuntu.id
-  instance_type   = "t3.micro"
-  security_groups = [aws_security_group.webSG.id]
-  user_data       = file("user_data.sh")
-  key_name        = "hw41"
+  block_device_mappings {
+    device_name = "/dev/sda1"
 
-  lifecycle {
-    create_before_destroy = true
+    ebs {
+      volume_size = 10
+    }
   }
+
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+
+  cpu_options {
+    core_count       = 2
+    threads_per_core = 1
+  }
+
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  disable_api_termination = true
+
+  ebs_optimized = true
+
+    iam_instance_profile {
+    name = "WebServerDiplom"
+  }
+
+  image_id = data.aws_ami.latest_ubuntu.id
+
+  instance_initiated_shutdown_behavior = "terminate"
+
+  instance_type = "t3.micro"
+
+  key_name = "hw41"
+
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
+  monitoring {
+    enabled = false
+  }
+
+  network_interfaces {
+    associate_public_ip_address = true
+  }
+
+#   placement {
+#     availability_zone = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
+#   }
+
+  ram_disk_id = "test"
+
+  vpc_security_group_ids = [aws_security_group.webSG.id]
+
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "MyWebServer"
+    }
+  }
+
+  user_data = filebase64("${path.module}/user_data.sh")
 }
 
+# resource "aws_launch_configuration" "web" {
+#   //  name            = "WebServer-Highly-Available-LC"
+#   name_prefix     = "WebServer-Highly-Available-LC-"
+#   image_id        = data.aws_ami.latest_ubuntu.id
+#   instance_type   = "t3.micro"
+#   security_groups = [aws_security_group.webSG.id]
+#   user_data       = file("user_data.sh")
+#   key_name        = "hw41"
 
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+# }
+
+"aws_launch_template" "web"
 
 resource "aws_autoscaling_group" "web" {
-  name                 = "ASG-${aws_launch_configuration.web.name}"
-  launch_configuration = aws_launch_configuration.web.name
-  min_size             = 1
+  name                 = "ASG-${aws_launch_template.web.name}"
+#   launch_configuration = aws_launch_configuration.web.name
+  min_size             = 2
   max_size             = 2
-  min_elb_capacity     = 1
+  min_elb_capacity     = 2
   health_check_type    = "ELB"
   vpc_zone_identifier  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
   load_balancers       = [aws_elb.web.name]
-
+  launch_template {
+    id      = aws_launch_template.web.id
+    version = "$Latest"
+  }
   dynamic "tag" {
     for_each = {
       Name   = "WebServer in ASG"
