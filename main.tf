701083505d58a4762ddab0d6efbf8a8c5a754649
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-05 14:28:36 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -146,7 +146,7 @@ resource "aws_autoscaling_group" "web" {
   min_elb_capacity     = 2
 #   health_check_type    = "ELB"
   vpc_zone_identifier  = [aws_default_subnet.default_az1.id, aws_default_subnet.default_az2.id]
-#   load_balancers       = [aws_lb.weblb.name]
+  load_balancers       = [aws_lb.weblb.name]
   launch_template {
     id      = aws_launch_template.web.id
     version = "$Latest"
