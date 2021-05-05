ebeeae154d2e49b4a1628d6c3eb645d9921a9f36
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-05 17:41:13 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -161,7 +161,6 @@ resource "aws_lb" "weblb" {
   internal           = false
   load_balancer_type = "application"
   subnets            =  var.app_subnets
-  target_group_arn   = aws_lb_target_group.webtg.arn
 #   availability_zones = [data.aws_availability_zones.available.names[0], data.aws_availability_zones.available.names[1]]
   security_groups    = [aws_security_group.webSG.id]
   tags = {
