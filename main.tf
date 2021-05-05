186d1b214c3fa12d22f8c28a40d2af9951d82a62
Jeka-anik <71710408+Jeka-anik@users.noreply.github.com>
2021-05-05 21:31:39 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,10 @@
 provider "aws" {
     region     = "us-east-1"
 }
-
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
+}
 variable "app_subnets" { 
     type = list(string) 
     description = "App subnets id" 
